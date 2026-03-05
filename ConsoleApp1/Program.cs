e931294a308ddff1f9b62e5983bd19082ea0590b
Kerkar Gayitri (ENG19-Goa) <gayitri.kerkar@syntegon.com>
2026-03-05 11:13:28 +0530

    main

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+User u = new User()
+{ 
+    Id=1,
+    Name="Me"
+};
+
+Console.WriteLine(u.Id.ToString());
+Console.WriteLine(u.Name);
