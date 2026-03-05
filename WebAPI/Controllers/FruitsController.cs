d30910e088bfd9bc05c4b5ac851574cb149a4bb6
Kerkar Gayitri (ENG19-Goa) <gayitri.kerkar@syntegon.com>
2026-03-05 13:47:52 +0530

    WebAPI

---
+++ WebAPI/Controllers/FruitsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitsController : ControllerBase
+    {
+        public List<string> fruits = new List<string>()
+        {
+            "Apple",
+            "Orange",
+            "Guava",
+            "Mango",
+            "Banana"
+        };
+
+        [HttpGet]
+        public List<string> GetFruits()
+        {
+            return fruits;
+        }
+
+        [HttpGet("{id}")]
+        public string GetFruits(int id)
+        {
+            return fruits.ElementAt(id);
+        }
+
+    }
+}
